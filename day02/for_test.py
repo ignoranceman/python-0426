eb9f837d89272ae6f967600658c742fdcb29bf0e
shiyao19951113 <820591937@qq.com>
2019-04-28 09:18:29 +0800

    updates

---
+++ day02/for_test.py
@@ -0,0 +1,4 @@
+for i in range(0, 1000, 2):
+    print(i)
+else:
+    print(i + 100)
---
+++ day02/if_test.py
@@ -0,0 +1,22 @@
+score = 70
+
+if score > 60:
+    print("及格")
+else:
+    print("不及格")
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+else:
+    print("C")
+
+# for i in range(-3, 4):
+#     if i < 0:
+#         j = -i
+#     else:
+#         j = i
+#     print(' ' * j + 'x' * (7 - 2 * j))
+
+
